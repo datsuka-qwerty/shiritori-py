6dbc7d861977e10d24885172a5400ba00a183ff5
Datsuka <49899602+datsuka-qwerty@users.noreply.github.com>
2022-09-10 04:44:58 +0900

    [hotfix]fix shiritori.py

--- Shiritori.py
+++ Shiritori.py
@@ -1,4 +1,6 @@
 # coding: utf-8
+from cgi import print_arguments
+from operator import contains
 import sys
 from encodings import search_function
 
@@ -8,13 +10,12 @@ def main():
 	youon_dic = {"ァ": "ア", "ィ": "イ", "ゥ": "ウ", "ェ": "エ", "ォ": "オ","ッ": "ツ","ャ": "ヤ", "ュ": "ユ", "ョ": "ヨ", "ー": None} # 拗音・長音 変換用
 	youon_table = str.maketrans(youon_dic) # 変換用テーブル作成
 
-	used = open("./used.txt", "w+", encoding="utf-8") # used.txtを中身削除して新規オープン
-	print("シリトリ", file=used) # 初期値のシリトリを追加
+	used = ["シリトリ"]
 	# --ends here--
 
 	# --first time game--
-	user_in = input("Input start wtih リ in カタカナ. ->")
 
+	user_in = input("Input start wtih リ in カタカナ. ->")
 	if user_in in used: # 既に使用済みの単語を入力された場合
 		print(user_in+" is already used...")
 		print("YOU LOSE!!! \n I WIN!!!!!!!")
@@ -28,11 +29,12 @@ def main():
 	else:
 		pass
 
-	print(user_in, file=used) # 使用済みリストへ記述
+	used.append(user_in.replace("\n", ""))
 	print("You entered "+user_in, end=" ")
-	print("and the last letter is "+user_in[-1].translate(youon_table)) # 拗音で終わる場合、translateで変換
+	user_in_tr = user_in.translate(youon_table) # 拗音で終わる場合、translateで変換
+	print("and the last letter is "+user_in_tr[-1])
 
-	dic = open("./Dic/"+user_in[-1].translate(youon_table)+".txt", "r", encoding="utf-8") # dicへ対応する文字のtxtを開く
+	dic = open("./Dic/"+user_in_tr[-1]+".txt", "r", encoding="utf-8") # dicへ対応する文字のtxtを開く
 
 	cnt = 0 # 探索中の行を読み取るカウンタリセット
 
@@ -42,17 +44,17 @@ def main():
 	else:
 		pass
 
+	dic = open("./Dic/"+user_in_tr[-1]+".txt", "r", encoding="utf-8") # dicへ対応する文字のtxtを開く
+
 	# 回答用の単語を探していく
-	for searching_string in dic:
+	for searching_string in dic.readlines():
 		cnt = cnt + 1
 		if cnt == len(dic.readlines()): # 回答出来なかった場合ー＞ファイルの一番最後まで探索しても回答単語が見つからなかった場合
 			print("Oh no...... \n I have no idea... \n Congratulations!!! YOU WIN!!!")
 			sys.exit()
-		elif searching_string in used: # 探索中の単語が使用済みリストにいた場合、スキップ
-			pass
-		else: # 回答単語を見つけたら、tmpへ代入
+		elif searching_string.replace("\n", "") not in used:
 			tmp = searching_string
-			print(searching_string, file=used) # tmpを使用済みリストへ記述
+			used.append(searching_string.replace("\n", ""))
 			break
 
 	print("Well.... I'll say "+tmp) # 回答
@@ -61,7 +63,8 @@ def main():
 	# --loop game--
 	while(1):
 		tmp = tmp.replace("\n", "") # txtに含まれる改行コード削除
-		user_in = input("Input start with "+tmp[-1].translate(youon_table)+" in カタカナ. ->") # 入力要求
+		tmp_tr = tmp.translate(youon_table) # 拗音・長音 変換
+		user_in = input("Input start with "+tmp_tr[-1]+" in カタカナ. ->") # 入力要求
 
 		if user_in in used: # 使用済みリストにいる場合
 			print(user_in+" is already used...")
@@ -70,17 +73,18 @@ def main():
 		elif user_in[-1] == "ン": # ンで終わってる場合
 			print("OH.... YOU ENTERED ン.... \n YOU LOSE!!! \n I WIN!!!!!!!")
 			sys.exit()
-		elif user_in[0] != tmp[-1].translate(youon_table): # ルールを満たしていない場合
-			print(user_in+" is not starting with "+tmp[-1].translate(youon_table)+"... \n Please retry :-)")
+		elif user_in[0] != tmp_tr[-1]: # ルールを満たしていない場合
+			print(user_in+" is not starting with "+tmp_tr[-1]+"... \n Please retry :-)")
 			continue # whileの先頭へ戻る
 		else:
 			pass
 
-		print(user_in, file=used) # 使用済みリストへ追記
+		used.append(user_in.replace("\n", ""))
 		print("You entered "+user_in, end=" ")
-		print("and the last letter is "+user_in[-1].translate(youon_table))
+		user_in_tr = user_in.translate(youon_table) # 拗音で終わる場合、translateで変換
+		print("and the last letter is "+user_in_tr[-1])
 
-		dic = open("./Dic/"+user_in[-1].translate(youon_table)+".txt", "r", encoding="utf-8") # 対応する文字のtxtファイルを開く
+		dic = open("./Dic/"+user_in_tr[-1]+".txt", "r", encoding="utf-8") # 対応する文字のtxtファイルを開く
 
 		cnt = 0
 
@@ -90,16 +94,17 @@ def main():
 		else:
 			pass
 
-		for searching_string in dic:
+		dic = open("./Dic/"+user_in_tr[-1]+".txt", "r", encoding="utf-8") # dicへ対応する文字のtxtを開く
+
+		for searching_string in dic.readlines():
 			cnt = cnt + 1
+			print("fuck search "+searching_string.replace("\n", ""))
 			if cnt == len(dic.readlines()):
 				print("Oh no...... \n I have no idea... \n Congratulations!!! YOU WIN!!!")
 				sys.exit()
-			elif searching_string in used:
-				pass
-			else:
+			elif searching_string.replace("\n", "") not in used:
 				tmp = searching_string
-				print(searching_string, file=used)
+				used.append(searching_string.replace("\n", ""))
 				break
 
 		print("Well.... I'll say "+tmp)
---
+++ test.py
@@ -0,0 +1,10 @@
+from cgi import print_arguments
+import sys
+import os
+from encodings import search_function
+
+
+if "ミキ" in ['シリトリ', 'リンゴ', 'ゴロウ', 'ウミ', 'ミキ', 'キミ']:
+	print("there is")
+else:
+	print("NO")
